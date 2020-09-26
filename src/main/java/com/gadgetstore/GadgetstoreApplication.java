091e038e7e36e186c740dba58df2192492d7794e
willyaditya <willyaditya03@gmail.com>
2020-09-26 23:26:29 +0700

    Initial commit

---
+++ src/main/java/com/gadgetstore/GadgetstoreApplication.java
@@ -0,0 +1,13 @@
+package com.gadgetstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadgetstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GadgetstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gadgetstore/GadgetstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gadgetstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
